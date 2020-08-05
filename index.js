f1ac984779578c71eef152ba37d71bfa4a837b1c
Ricardo Castro <ricardocastro.ti@gmail.com>
2020-08-05 17:00:33 -0300

    Atividade 1 e 2 Concluidas

--- index.js
+++ index.js
@@ -2,10 +2,18 @@ import express from 'express';
 import router from './route/grade.js';
 import { promises as fs } from 'fs';
 
+//Variáveis Globais
+global.fileName = 'grades.json';
+
 const app = express();
 app.use(express.json());
 app.use('/', router);
 
-app.listen(3000, () => {
-  console.log('Api OK');
+app.listen(3000, async () => {
+  try {
+    await fs.readFile(global.fileName);
+    console.log(`Arquivo: ${global.fileName} lido com sucesso!`);
+  } catch (error) {
+    console.log(error);
+  }
 });
--- route/grade.js
+++ route/grade.js
@@ -1,8 +1,57 @@
 import express from 'express';
+import { promises as fs } from 'fs';
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  res.send('<p>Teste 2</p>');
+const dateTime = new Date();
+
+router.get('/', async (req, res) => {
+  try {
+    const bodyData = req.body;
+    const dataJson = await fs.readFile(global.fileName);
+    const data = JSON.parse(dataJson);
+    const obj = {
+      id: data.nextId,
+      student: bodyData.student,
+      subject: bodyData.subject,
+      type: bodyData.type,
+      value: bodyData.value,
+      timestamp: dateTime.toJSON(),
+    };
+
+    data.grades.push(obj);
+    data.nextId++;
+    await fs.writeFile(global.fileName, JSON.stringify(data, null, 2), 'utf-8');
+
+    res.send(data);
+    //res.send(`<p>${JSON.stringify(bodyData, null, ' ')}</p>`);
+  } catch (error) {
+    console.log(error);
+  }
 });
 
+router.patch('/update/:id', async (req, res) => {
+  try {
+    const bodyData = req.body;
+    const dataJson = await fs.readFile(global.fileName);
+    const data = JSON.parse(dataJson);
+    const obj = data.grades.findIndex((infos) => {
+      return infos.id === parseInt(req.params.id);
+    });
+
+    if (obj === -1) {
+      throw new Error('Id Inválido');
+    }
+    console.log(bodyData.student);
+    console.log(data.grades[obj].student);
+    data.grades[obj].student = bodyData.student;
+    data.grades[obj].subject = bodyData.subject;
+    data.grades[obj].type = bodyData.type;
+    data.grades[obj].value = bodyData.value;
+
+    await fs.writeFile(global.fileName, JSON.stringify(data, null, 2), 'utf-8');
+    res.send(data.grades[obj]);
+  } catch (error) {
+    console.log(error);
+  }
+});
 export default router;
